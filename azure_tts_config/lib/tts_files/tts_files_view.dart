4051d9b7a0fa0066a867182660125add321099f5
hss01248 <hss012489@gmail.com>
2024-04-24 16:51:08 +0800

    解决布局报错问题

--- azure_tts_config/lib/tts_files/tts_files_view.dart
+++ azure_tts_config/lib/tts_files/tts_files_view.dart
@@ -17,14 +17,16 @@ class TtsFilesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("TTS Files(点击播放,长按分享)"),),
+      appBar: AppBar(
+        title: const Text("TTS Files(点击播放,长按分享)"),
+      ),
       body: GetBuilder<TtsFilesLogic>(
         builder: (controller) {
           return ListView.builder(
-            itemCount: state.files.length,
-              itemBuilder: (context,index){
-            return buildItem(index);
-          });
+              itemCount: state.files.length,
+              itemBuilder: (context, index) {
+                return buildItem(index);
+              });
         },
       ),
     );
@@ -32,38 +34,68 @@ class TtsFilesPage extends StatelessWidget {
 
   Widget buildItem(int index) {
     String path = state.files[index];
+
+    return Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            AudioPlayerIconWidget(
+              path: path,
+            ).marginOnly(left: 10),
+            Flexible(child: buildRight(path)),
+          ],
+        ),
+        Divider(thickness: 1,height: 1,color: Color(0xffe8e8e8),).marginSymmetric(horizontal: 16)
+      ],
+    );
+  }
+
+  Widget buildRight(String path) {
     File file = File(path);
-    String name = path.substring(path.lastIndexOf("/")+1);
-    String size = "${NumberFormat('###.0').format(file.lengthSync()/1024.0)}KB";
-    String duration = "${NumberFormat('###.0').format(file.lengthSync()/6/1024)}s";//48k bit/s
+    String name = path.substring(path.lastIndexOf("/") + 1);
+    String size =
+        "${NumberFormat('###.0').format(file.lengthSync() / 1024.0)}KB";
+    String duration =
+        "${NumberFormat('###.0').format(file.lengthSync() / 6 / 1024)}s"; //48k bit/s
     final dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
     final formattedDateTime = dateFormat.format(file.lastModifiedSync());
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        AudioPlayerIconWidget(path: path,).marginOnly(left: 10),
-        Expanded(child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(name,maxLines: 2,softWrap: true,),
-            const SizedBox(width: 12,),
-            Row(
-              children: [
-                Text(size),
-                const SizedBox(width: 10,),
-                Text(duration),
-              ],
-            )
+            Flexible(
+                flex: 1,
+                child: Text(
+                  name,
+                  maxLines: 2,
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                )),
           ],
-        )).marginAll(10).gestures(
-    behavior: HitTestBehavior.opaque,
-            onLongPress: () async {
+        ),
+        SizedBox(height: 5,),
+        Row(
+          children: [
+            Text(size),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(duration),
+          ],
+        )
+      ],
+    ).marginAll(10).gestures(
+        behavior: HitTestBehavior.opaque,
+        onLongPress: () async {
           //长按分享
-          final result = await Share.shareXFiles([XFile(path)], text: 'amazing azure tts:');
+          final result = await Share.shareXFiles([XFile(path)],
+              text: 'amazing azure tts:');
           if (result.status == ShareResultStatus.success) {
             print('Thank you for sharing the tts');
           }
-        })
-      ],
-    );
+        });
   }
 }
